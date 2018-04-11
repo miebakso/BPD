161f34f554ef94034d1a8570cf7e2364a522b928
miebakso <matthew_ariel_wangsit@yahoo.com>
2018-04-11 14:49:22 +0700

    added

---
+++ static/header.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="static/plugins/images/favicon.png">
+    <title>Mie Admin - MieMie</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="static/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Menu CSS -->
+    <link href="static/plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css" rel="stylesheet">
+    <!-- toast CSS -->
+    <link href="static/plugins/bower_components/toast-master/css/jquery.toast.css" rel="stylesheet">
+    <!-- morris CSS -->
+    <link href="static/plugins/bower_components/morrisjs/morris.css" rel="stylesheet">
+    <!-- animation CSS -->
+    <link href="static/css/animate.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="static/css/style.css" rel="stylesheet">
+    <!-- color CSS -->
+    <link href="static/css/colors/blue-dark.css" id="theme" rel="stylesheet">
+</head>
---
+++ static/script.php
@@ -0,0 +1,32 @@
+<script src="static/plugins/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="static/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Menu Plugin JavaScript -->
+<script src="static/plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+<!--slimscroll JavaScript -->
+<script src="static/js/jquery.slimscroll.js"></script>
+<!--Wave Effects -->
+<script src="static/js/waves.js"></script>
+<!--Counter js -->
+<script src="static/plugins/bower_components/waypoints/lib/jquery.waypoints.js"></script>
+<script src="static/plugins/bower_components/counterup/jquery.counterup.min.js"></script>
+<!--Morris JavaScript -->
+<script src="static/plugins/bower_components/raphael/raphael-min.js"></script>
+<script src="static/plugins/bower_components/morrisjs/morris.js"></script>
+<!-- Custom Theme JavaScript -->
+<script src="static/js/custom.min.js"></script>
+<script src="static/js/dashboard1.js"></script>
+<script src="static/plugins/bower_components/toast-master/js/jquery.toast.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    $.toast({
+        heading: 'Welcome to Mie admin',
+        text: 'Use the predefined ones, or specify a custom position object.',
+        position: 'top-right',
+        loaderBg: '#ff6849',
+        icon: 'info',
+        hideAfter: 3500,
+        stack: 6
+    })
+});
+</script>
