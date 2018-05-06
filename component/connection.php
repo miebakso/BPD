1c6958c30f2e01767734a44455af6df2a02290ea
ThobyGithub <gabriel.tobby@yahoo.co.id>
2018-05-06 17:49:01 +0700

    update report page

--- component/connection.php
+++ component/connection.php
@@ -1,10 +1,6 @@
 <?php
 	try {
-<<<<<<< HEAD
 		$conn = new PDO("sqlsrv:server=DESKTOP-8GFM4DQ\SQLEXPRESS;database=DB_Mie","","");
-=======
-		$conn = new PDO("sqlsrv:server=DESKTOP-D12RCSK;Database=DB_Mie","","");
->>>>>>> c0e64538de485b0704ea8deff1c09e610b68b9d3
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch(Exception $e) {
 		die(print_r($e->getMessage() ));
--- show_current_info.php
+++ show_current_info.php
@@ -1,17 +1,74 @@
-<?php
-    $start_date = $_POST['start_date'];
-    $end_date = $_POST['end_date'];
 
-    $result = $conn->query("select datediff('$start_date','$end_date')");
-    $result->nextRowset();
+<html lang="en">
+
+<?php require "component/header.php" ?>
+
+<body>
     
-    if($result<0){
-        $state = 'SHOW failed! because end date less than start date';
-    }
-    else{
-        $state = 'SUCCESS!';
-    }
+    <div id="wrapper">
+        <!-- Navigation -->
+        <?php require "component/navigation.php" ?>
+        <!-- Sidebar -->
+        <?php require "component/sidebar.php" ?>
 
-    header('Location: http://localhost/PBD/index.php?&result='.$state);
-    die();
-?>
+        <?php require "component/connection.php" ?>
+        
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row bg-title">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        <h4 class="page-title">DETAIL REPORT</h4> 
+                    </div>
+                </div>
+
+                <!--row -->
+                <div class="row">
+               
+                        <div class="col-sm-6 col-md-offset-3">
+                            <div class="white-box">
+                                <?php 
+                                    $query = "select * from CustomerOrder where order_date >= '".$_POST ['start_date']."' AND order_date <= '".$_POST['end_date']."'";
+                                    $result = $conn->query($query);
+                                    $final = $result->fetch();
+
+                                    echo '
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Order Date</th>
+                                            <th>Customer Name</th>
+                                            <th>Total Price</th>
+                                            <th><center>Total Discounted Price</center></th>
+                                        </tr>
+                                    ';
+                                      
+                                    foreach ($result as $record) {
+                                        echo '
+                                            <tr>
+                                                <td>'.$record['ID'].'</td>
+                                                <td>'.$record['order_date'].'</td>
+                                                <td>'.$record['customer_name'].'</td>
+                                                <td class="success"><center><b>'.$record['total_price'].'</b></center></td>
+                                                <td class="danger"><center><b>'.$record['total_discounted_price'].'</b></center></td>
+                                                <td>'.$record['total_discounted_price'].'</td>
+                                            </tr>';
+                                    }
+                                         
+                                ?>
+                            </div>
+                        
+                    </div>
+                </div>
+
+            </div>
+            <!-- /.container-fluid -->
+            <footer class="footer text-center"> 2018 &copy; P - Mie </footer>
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper 12321321-->
+    <!-- jQuery -->
+    <?php require "component/script.php" ?>
+</body>
+
+</html>
